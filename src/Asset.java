fd21b20c3b7ca197ec1f5088ca1995a80f725b25
nay-aa <kaban@cwu.edu>
2026-05-15 12:27:18 -0700

    Created properties for each class along with setters and getters

--- src/Asset.java
+++ src/Asset.java
@@ -1,3 +1,87 @@
-public class Asset { }
+public class Asset {
+    private int asset_ID;
+    private int portfolio_ID;
+    private String assetName;
+    private String assetType;
+    private int allocationPercentage;
+    private int amount;
 
-// test
+
+    public Asset(
+        int asset_ID,
+        int portfolio_ID,
+        String assetName,
+        String assetType,
+        int allocationPercentage,
+        int amount
+    ){
+        this.asset_ID = asset_ID;
+        this.portfolio_ID = portfolio_ID;
+        this.assetName = assetName;
+        this.assetType = assetType;
+        this.allocationPercentage = allocationPercentage;
+        this.amount = amount;
+    }
+
+
+    public int getAsset_ID() {
+        return asset_ID;
+    }
+
+
+    public void setAsset_ID(int asset_ID) {
+        this.asset_ID = asset_ID;
+    }
+
+
+    public int getPortfolio_ID() {
+        return portfolio_ID;
+    }
+
+
+    public void setPortfolio_ID(int portfolio_ID) {
+        this.portfolio_ID = portfolio_ID;
+    }
+
+
+    public String getAssetName() {
+        return assetName;
+    }
+
+
+    public void setAssetName(String assetName) {
+        this.assetName = assetName;
+    }
+
+
+    public String getAssetType() {
+        return assetType;
+    }
+
+
+    public void setAssetType(String assetType) {
+        this.assetType = assetType;
+    }
+
+
+    public int getAllocationPercentage() {
+        return allocationPercentage;
+    }
+
+
+    public void setAllocationPercentage(int allocationPercentage) {
+        this.allocationPercentage = allocationPercentage;
+    }
+
+
+    public int getAmount() {
+        return amount;
+    }
+
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+ }
+
--- src/Portfolio.java
+++ src/Portfolio.java
@@ -1,1 +1,73 @@
-public class Portfolio { }
+public class Portfolio {
+    private int portfolio_ID;
+    private int user_ID;
+    private String portfolioName;
+    private double totalValue;
+    private String riskLevel;
+
+
+    public Portfolio(
+        int portfolio_ID,
+        int user_ID,
+        String portfolioName,
+        double totalValue,
+        String riskLevel
+    ){
+        this.portfolio_ID = portfolio_ID;
+        this.user_ID = user_ID;
+        this.portfolioName = portfolioName;
+        this.totalValue = totalValue;
+        this.riskLevel = riskLevel;
+    }
+
+
+    public int getPortfolio_ID() {
+        return portfolio_ID;
+    }
+
+
+    public void setPortfolio_ID(int portfolio_ID) {
+        this.portfolio_ID = portfolio_ID;
+    }
+
+
+    public int getUser_ID() {
+        return user_ID;
+    }
+
+
+    public void setUser_ID(int user_ID) {
+        this.user_ID = user_ID;
+    }
+
+
+    public String getPortfolioName() {
+        return portfolioName;
+    }
+
+
+    public void setPortfolioName(String portfolioName) {
+        this.portfolioName = portfolioName;
+    }
+
+
+    public double getTotalValue() {
+        return totalValue;
+    }
+
+
+    public void setTotalValue(double totalValue) {
+        this.totalValue = totalValue;
+    }
+
+
+    public String getRiskLevel() {
+        return riskLevel;
+    }
+
+
+    public void setRiskLevel(String riskLevel) {
+        this.riskLevel = riskLevel;
+    }
+
+ }
