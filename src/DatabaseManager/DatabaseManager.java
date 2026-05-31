98b4eff0281812a18d5528abd9a27549ec118ec8
SteveBuerstatteCWU <buerstattes@cwu.edu>
2026-05-31 15:17:48 -0700

    updated database manager

--- src/DatabaseManager/DatabaseManager.java
+++ src/DatabaseManager/DatabaseManager.java
@@ -11,7 +11,7 @@ public class DatabaseManager {
 
     private static final String URL = "jdbc:mysql://localhost:3306/portfolioapp";
     private static final String USER = "root";
-    private static final String PASSWORD = "ROOTPASSWORDS";
+    private static final String PASSWORD = "RootUser420";
 
     /**
      * 
